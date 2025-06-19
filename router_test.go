8eebae103a7bc5fe5db89d4979fa5c912a8bf7ff
maton369 <ysk901085@gmail.com>
2025-06-19 11:57:36 +0900

    Add: RouterのSearchテストを追加

--- router_test.go
+++ router_test.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -38,6 +39,51 @@ func TestRouter_GET(t *testing.T) {
 	for _, testcase := range testcases {
 		t.Run(testcase.name, func(t *testing.T) {
 			r.GET(testcase.endpoint, testcase.handler)
+		})
+	}
+}
+
+func TestRouter_Search(t *testing.T) {
+	testcases := []struct {
+		name     string
+		endpoint string
+		handler  http.Handler
+	}{
+		{
+			name:     "/のエンドポイントのハンドラを取得する",
+			endpoint: "/",
+			handler:  http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}),
+		},
+		{
+			name:     "/hoge/fugaのエンドポイントのハンドラを取得する",
+			endpoint: "/hoge/fuga",
+			handler:  http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}),
+		},
+		{
+			name:     "/helloのエンドポイントのハンドラを取得する",
+			endpoint: "/hello",
+			handler:  http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}),
+		},
+		{
+			name:     "/hogeのエンドポイントのハンドラを取得する",
+			endpoint: "/hoge",
+			handler:  http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}),
+		},
+	}
+
+	r := NewRouter()
+
+	for _, testcase := range testcases {
+		r.GET(testcase.endpoint, testcase.handler)
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			handler := r.Search(http.MethodGet, testcase.endpoint)
+			// 関数のポインタを比較する
+			if reflect.ValueOf(handler).Pointer() != reflect.ValueOf(testcase.handler).Pointer() {
+				t.Errorf("ハンドラが異なります\nexpected: %v\nactual: %v", testcase.handler, handler)
+			}
 		})
 	}
 }
